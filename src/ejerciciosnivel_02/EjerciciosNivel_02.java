8ec7d475098a5f5bce548eb3ab5a49f293f26e3c
Joel Garcia Valhondo <joel.garcia.wk@gmail.com>
2022-01-18 20:10:42 +0100

    Añadido ejercicio strStr.

--- src/ejerciciosnivel_02/EjerciciosNivel_02.java
+++ src/ejerciciosnivel_02/EjerciciosNivel_02.java
@@ -54,6 +54,32 @@ public class EjerciciosNivel_02 {
         return contador;
     }
     
+    public int strStr(String str1, String str2){
+        //EN LA VIDA REAL SE SA .find PARA QUE TE DEVUELVA EL VALOR.
+        int posicion = -1;
+        for(int i=0; i < str1.length(); i++){
+            if(str1.charAt(i) == str2.charAt(0)){
+                posicion = i;
+                int j=0;
+                while((j < str2.length() && i < str1.length()) && str1.charAt(i) == str2.charAt(j)){
+                    i++;
+                    j++;                       
+                }
+                if(j == str2.length()){ //encuentra la palabra.
+                    return posicion;
+                }
+                if(i == str1.length()){ //se termina la palabra 1 y no cabe la 2.
+                     return -1;
+                }
+                //si llega aqui es porque ha salido del while porque las letras
+                //son diferentes.
+                i = posicion;
+                posicion =-1;
+            }
+        }
+        return posicion;
+    }
+    
     public static void main(String[] args) {
         //test primer ejercicio.
         char[][] listaPalabras = {
@@ -68,14 +94,18 @@ public class EjerciciosNivel_02 {
         EjerciciosNivel_02 e = new EjerciciosNivel_02();
         System.out.println(e.esEscaleraDePalabras(listaPalabras));
         //test segundo ejercicio.
-        System.out.println(e.costeErroresADN("ACGT", "TGCA"));
-        System.out.println(e.costeErroresADN("A-C-G-T-ACGT", "TTGGCCAATGCA"));
-        System.out.println(e.costeErroresADN("AAAAAAAA", "TTTATTTT"));
-        System.out.println(e.costeErroresADN("GATTACA", "CTATT-T"));
-        System.out.println(e.costeErroresADN("CAT-TAG-ACT", "GTATATCCAAA"));
-        System.out.println(e.costeErroresADN("--------", "ACGTACGT"));
-        System.out.println(e.costeErroresADN("TAATAA", "ATTATT"));
-        System.out.println(e.costeErroresADN("GGGA-GAATATCTGGACT", "CCCTACTTA-AGACCGGT"));
+        System.out.println(e.costeErroresADN("ACGT", "TGCA")); //0
+        System.out.println(e.costeErroresADN("A-C-G-T-ACGT", "TTGGCCAATGCA")); //8
+        System.out.println(e.costeErroresADN("AAAAAAAA", "TTTATTTT")); //1
+        System.out.println(e.costeErroresADN("GATTACA", "CTATT-T")); //3
+        System.out.println(e.costeErroresADN("CAT-TAG-ACT", "GTATATCCAAA")); // 6
+        System.out.println(e.costeErroresADN("--------", "ACGTACGT")); //16
+        System.out.println(e.costeErroresADN("TAATAA", "ATTATT")); //0
+        System.out.println(e.costeErroresADN("GGGA-GAATATCTGGACT", "CCCTACTTA-AGACCGGT")); //6
+        //test tercer ejercicio.
+        System.out.println(e.strStr("HOLA_MUNDO", "MU")); //5
+        System.out.println(e.strStr("HOLA_MUNDO", "ME")); //-1
+        System.out.println(e.strStr("HOLA_MUNDO", "DO")); //8
     }
     
 }
